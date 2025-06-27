2f8771fbb173e7322cd04dcbde328f6244f55c36
GhadaYoussefGY <ghada.youssef@pharos-solutions.de>
2025-06-27 19:08:13 +0300

    fix sidekiq scheduler to run SendDueDateRemindersJob every minute

--- app/jobs/send_due_date_reminders_job.rb
+++ app/jobs/send_due_date_reminders_job.rb
@@ -2,6 +2,7 @@ class SendDueDateRemindersJob < ApplicationJob
   queue_as :default
 
   def perform
+    Rails.logger.info "[SendDueDateRemindersJob] Triggered by Sidekiq schedule"
     User.where(send_due_date_reminder: true).find_each do |user|
       Time.use_zone(user.time_zone) do
         now = Time.zone.now
@@ -12,6 +13,7 @@ class SendDueDateRemindersJob < ApplicationJob
         tickets = user.assigned_tickets.where(due_date: due_date)
 
         tickets.each do |ticket|
+          Rails.logger.info("found a ticket")
           ReminderMailer.due_soon_reminder(user, ticket).deliver_later
           PushNotificationService.send_due_reminder(user, ticket)
         end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,13 @@
+require 'sidekiq'
 require 'sidekiq/scheduler'
 
 Sidekiq.configure_server do |config|
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(Rails.root.join('config/sidekiq.yml'))
-    Sidekiq::Scheduler.reload_schedule!
+    schedule_file = Rails.root.join('config/sidekiq.yml')
+    if File.exist?(schedule_file)
+      Sidekiq.logger.info "Loading schedule from #{schedule_file}"
+      Sidekiq.schedule = YAML.load_file(schedule_file)
+      Sidekiq::Scheduler.reload_schedule!
+    end
   end
 end
